ada0e8facca5a9efe1b37a9938188f4d32e22a0c
kishorereddypelleti2000 <kishorereddypelleti12@gmail.com>
2026-06-04 19:24:28 +0530

    Update appasg.tf

--- appasg.tf
+++ appasg.tf
@@ -22,7 +22,7 @@ resource "aws_launch_template" "swiggy-app-template" {
   name_prefix   = "swiggy-app-template"
   image_id      = "ami-0685bcc683dadb6b9"
   instance_type = "t3.micro"
-  key_name      = "Kubeadm0"
+  key_name      = "Kubeadmm"
 
   network_interfaces {
     security_groups             = [aws_security_group.swiggy-ec2-asg-sg-app.id]
