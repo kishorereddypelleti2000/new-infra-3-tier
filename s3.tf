924fbaaba60ff1009c6f73500a8b84ec6301e5c5
kishorereddypelleti2000 <kishorereddypelleti12@gmail.com>
2026-06-04 10:29:22 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -3,14 +3,7 @@ resource "aws_s3_bucket" "example_bucket" {
   bucket = "swiggy-bucket-terraform-0099demo-s"
 
   # Enable versioning
- # versioning {
-  #  enabled = true
- # }
-}
-resource "aws_s3_bucket_versioning" "example_bucket_versioning" {
-  bucket = aws_s3_bucket.example_bucket.id
-
-  versioning_configuration {
-    status = "Enabled"
+  versioning {
+    enabled = true
   }
 }
